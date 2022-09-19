1f7bd10673615770960fa3d279dbb2fe89510e25
Kamshinen Dewan <Kamshinen.Dewan@Sterling.ng>
2022-09-19 16:34:42 +0100

    first commit

---
+++ JS/script.js
@@ -0,0 +1,53 @@
+const prevBtns = document.querySelectorAll(".btn-prev");
+const nextBtns = document.querySelectorAll(".btn-next");
+const progress = document.getElementById('progress');
+const formSteps = document.querySelectorAll(".form-step")
+const progressSteps = document.querySelectorAll(".progress-step")
+
+let formStepsNum =0;
+
+nextBtns.forEach(btn=>{
+    btn.addEventListener('click', ()=>{
+        formStepsNum++; //increases the form steps before updating
+        updateFormSteps();
+        updateProgressBar();
+    })
+})
+
+prevBtns.forEach(btn=>{
+    btn.addEventListener('click', ()=>{
+        formStepsNum--; //decreases the form steps before updating
+        updateFormSteps();
+        updateProgressBar();
+    })
+})
+
+//updating function
+function updateFormSteps(){
+    //seek to find if there is any form-step that contain 'form-step-active' group
+    //if there is any, remove the className
+    formSteps.forEach(formStep=>{
+        formStep.classList.contains('form-step-active') &&
+        formStep.classList.remove("form-step-active"); 
+    })
+    formSteps[formStepsNum].classList.add("form-step-active");
+    
+    const progressActive = document.querySelectorAll(".progress-step-active");
+    const percentage = document.getElementById('percentage')
+    percentage.innerHTML = `${(formStepsNum/formSteps.length)*100 + '%'}  completed`;   
+}
+
+function updateProgressBar(){
+    progressSteps.forEach((progressStep, index)=>{
+        if(index < formStepsNum +1){
+            progressStep.classList.add('progress-step-active')
+        } else {
+            progressStep.classList.remove('progress-step-active')
+        }
+    })
+
+    const progressActive = document.querySelectorAll(".progress-step-active");
+
+    progress.style.width=((progressActive.length -1)/ (progressSteps.length-1)) *100 +"%";
+};
+
