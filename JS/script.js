14e4c8d0bea1118b0df92cb304aae5081ed9fda0
Kamshinen Dewan <Kamshinen.Dewan@Sterling.ng>
2022-09-21 00:34:33 +0100

    second commit

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,11 @@ const nextBtns = document.querySelectorAll(".btn-next");
 const progress = document.getElementById('progress');
 const formSteps = document.querySelectorAll(".form-step")
 const progressSteps = document.querySelectorAll(".progress-step")
+const submit = document.getElementById('submitbtn');
+const form = document.getElementById('form');
+const confirmation = document.getElementById('confirm');
+const firstName= document.getElementById('Firstname');
+const nameConfirmation = document.getElementById('name');
 
 let formStepsNum =0;
 
@@ -22,6 +27,16 @@ prevBtns.forEach(btn=>{
     })
 })
 
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    form.style.display = 'none';
+    confirmation.style.display= 'block';
+    nameConfirmation.innerHTML = firstName.value
+    // console.log(firstName.value)
+})
+
+
+
 //updating function
 function updateFormSteps(){
     //seek to find if there is any form-step that contain 'form-step-active' group
@@ -32,7 +47,7 @@ function updateFormSteps(){
     })
     formSteps[formStepsNum].classList.add("form-step-active");
     
-    const progressActive = document.querySelectorAll(".progress-step-active");
+    // const progressActive = document.querySelectorAll(".progress-step-active");
     const percentage = document.getElementById('percentage')
     percentage.innerHTML = `${(formStepsNum/formSteps.length)*100 + '%'}  completed`;   
 }
@@ -50,4 +65,3 @@ function updateProgressBar(){
 
     progress.style.width=((progressActive.length -1)/ (progressSteps.length-1)) *100 +"%";
 };
-
